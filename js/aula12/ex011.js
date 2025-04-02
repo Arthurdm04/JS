e8215173c95f8404504daf18dcf631e6a024a4d6
Arthur Dias Moresco <morescoarthur@gmail.com>
2025-04-02 11:15:12 -0300

    teste

--- js/aula12/ex011.js
+++ js/aula12/ex011.js
@@ -1,7 +1,7 @@
 var idade = 20
 if (idade < 16) {
     console.log('Não vota')}
-    else if(idade < 18 || idade > 65) {
+    else if(idade < 18 || idade > 66) {
         console.log('Voto opcional')
     } else {
         console.log('Voto obrigaório')
