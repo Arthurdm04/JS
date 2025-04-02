b75831958764683db3b773cf4d094f69f0168f56
Arthur Dias Moresco <135710271+Arthurdm04@users.noreply.github.com>
2025-04-02 11:11:49 -0300

    Add files via upload

---
+++ js/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log('Você foi Multado por excesso de velocidade')   
+}
+console.log('Dirija com cuidade e use o cinto de segurança')
---
+++ js/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do { // faça isso
+    console.log(`paso ${c}`)
+    c++ // c = c + 1
+} while (c <= 47) // enquanto isso for verdade 
+
+/*
+Exemplo:
+fatia da pizza com 8 pedaços:
+comeu um pedaço ele repete até chegga no último pedaço,
+qunado acabar os 8 pedaços, ele irá finalizar.
+*/
+
+
+/* var c = 1
+while(c <= 5) {
+    console.log(`paso ${c}`)
+    c++ // c = c + 1
+
+}*/
